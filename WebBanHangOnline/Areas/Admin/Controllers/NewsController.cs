91cbf688e586c313b8fbb217326a3c1a6aa78b3e
vtran <quangvutran61@gmail.com>
2024-03-20 02:53:21 +0700

    Add project files.

---
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,128 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/News
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 10; // Number of items per page
+            int pageNumber = (page ?? 1);
+
+            // Assuming dbContext is your EF context instance
+            var newsItems = db.News.OrderByDescending(n => n.Id);
+
+            // Paginate the news items
+            var pagedNewsItems = newsItems.ToPagedList(pageNumber, pageSize);
+
+            return View(pagedNewsItems);
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.CategoryId = 3;
+                model.ModifierDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.News.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ModifierDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.News.Find(id);
+            if (item != null)
+            {
+                db.News.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+            var item = db.News.Find(id);
+            if (item != null)
+            {
+                item.IsActive = !item.IsActive;
+                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, isAcive = item.IsActive });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.News.Find(Convert.ToInt32(item));
+                        db.News.Remove(obj);
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+    }
+}
---
+++ WebBanHangOnline/Areas/Admin/Controllers/Proxy/ProductCategoryProxy.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    // Interface cho Proxy và Real Subject
+    public interface IProductCategoryController
+    {
+        ActionResult Index();
+        ActionResult Add();
+        ActionResult Add(ProductCategory model);
+        ActionResult Edit(int id);
+        ActionResult Edit(ProductCategory model);
+    }
+
+    // Real Subject
+    public class RealProductCategoryController : Controller, IProductCategoryController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var items = db.ProductCategories;
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ProductCategory model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifierDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.ProductCategories.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.ProductCategories.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProductCategory model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ModifierDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.ProductCategories.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+
+    // Proxy
+    public class ProductCategoryControllerProxy : IProductCategoryController
+    {
+        private readonly RealProductCategoryController _realProductCategoryController;
+
+        public ProductCategoryControllerProxy()
+        {
+            _realProductCategoryController = new RealProductCategoryController();
+        }
+
+        // Một số logic kiểm tra quyền truy cập có thể được thêm vào các phương thức dưới đây
+
+        public ActionResult Index()
+        {
+            // Thực hiện kiểm tra quyền truy cập trước khi gọi phương thức Index() của Real Subject
+            return _realProductCategoryController.Index();
+        }
+
+        public ActionResult Add()
+        {
+            // Thực hiện kiểm tra quyền truy cập trước khi gọi phương thức Add() của Real Subject
+            return _realProductCategoryController.Add();
+        }
+
+        public ActionResult Add(ProductCategory model)
+        {
+            // Thực hiện kiểm tra quyền truy cập trước khi gọi phương thức Add(model) của Real Subject
+            return _realProductCategoryController.Add(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            // Thực hiện kiểm tra quyền truy cập trước khi gọi phương thức Edit(id) của Real Subject
+            return _realProductCategoryController.Edit(id);
+        }
+
+        public ActionResult Edit(ProductCategory model)
+        {
+            // Thực hiện kiểm tra quyền truy cập trước khi gọi phương thức Edit(model) của Real Subject
+            return _realProductCategoryController.Edit(model);
+        }
+    }
+}
